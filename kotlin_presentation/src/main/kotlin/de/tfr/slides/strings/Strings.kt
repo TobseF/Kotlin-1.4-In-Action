f616556d167d08bd27072259807224368be15375
Tobse <Tobse4Git@gmail.com>
2019-11-25 18:56:33 +0100

    ✨ Add Strings example

---
+++ kotlin_presentation/src/main/kotlin/de/tfr/slides/strings/Strings.kt
@@ -0,0 +1,16 @@
+package de.tfr.slides.strings
+
+
+val errors = 2
+
+val logMessage = "🐞 This error occurred $errors time at ${System.currentTimeMillis()}"
+
+val htmlSnippet = """
+    <h1>🦖 Die Dinos</h1>
+    <ul>
+      <li>Earl</li>
+      <li>Fran</li>
+      <li>Baby</li>
+    </ul>
+</li>
+""".trimIndent()
