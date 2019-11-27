fa5a9a10b3b876ffc24d73801a2ff7f40b5d0fdd
Tobse <Tobse4Git@gmail.com>
2019-11-27 14:27:42 +0100

    📝 Fix typo in lamda example

--- kotlin_presentation/src/main/kotlin/de/tfr/slides/lamdas/Lamdas.kt
+++ kotlin_presentation/src/main/kotlin/de/tfr/slides/lamdas/Lamdas.kt
@@ -2,10 +2,10 @@ package de.tfr.slides.lamdas
 
 import java.util.*
 
-fun localize(messageKey: String, translateAble: (String, Locale) -> String): String {
-    return translateAble.invoke(messageKey, getCurentUserLocale())
+fun localize(messageKey: String, translateable: (String, Locale) -> String): String {
+    return translateable.invoke(messageKey, getCurrentUserLocale())
 }
 
 fun message(message: String, locale: Locale) = "Lorem ipsum"
 
-fun getCurentUserLocale() = Locale.GERMANY
+fun getCurrentUserLocale(): Locale = Locale.GERMANY
